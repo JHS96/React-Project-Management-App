9255ec9dda9bdbfc2706a0b65d03f4f921b7edd8
JHS96 <jschaper@hotmail.co.za>
2025-05-27 15:01:45 +0200

    Display project names in ProjectList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 import ProjectList from './components/ProjectList.jsx';
 import ProjectDetails from './components/ProjectDetails.jsx';
 
 function App() {
+  const [projects, setProjects] = useState([
+    { projectName: 'Project 1' },
+    { projectName: 'Project 2' },
+  ]);
+
   return (
     <>
       <main className='h-screen mt-8 flex gap-8'>
-        <ProjectList />
+        <ProjectList projects={projects} />
         <ProjectDetails />
       </main>
     </>
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,13 +1,25 @@
-export default function ProjectList() {
+export default function ProjectList({ projects }) {
   return (
     <section className='bg-stone-900 rounded-tr-2xl'>
       <div className='pl-16 w-96'>
         <h2 className='mt-20 mb-10 uppercase text-3xl font-semibold text-white'>
           Your Projects
         </h2>
-        <button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>
+        <button className='px-6 py-2 rounded-md bg-stone-700 text-stone-400 hover:bg-stone-950'>
           + Add Project
         </button>
+        <ul className='mt-10'>
+          {projects.map((project, index) => {
+            return (
+              <li
+                key={project.projectName + index}
+                className='text-stone-400 text-lg py-2'
+              >
+                {project.projectName}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </section>
   );
