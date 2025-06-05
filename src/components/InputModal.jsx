4889ce38aee5800a39633247c73676e6186f99bf
JHS96 <jschaper@hotmail.co.za>
2025-06-05 13:11:25 +0200

    Unknown/unused import removed

--- src/components/InputModal.jsx
+++ src/components/InputModal.jsx
@@ -1,4 +1,4 @@
-import { act, forwardRef, useImperativeHandle, useRef, useState } from 'react';
+import { forwardRef, useImperativeHandle, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 export default forwardRef(function InputModal({ save }, ref) {
